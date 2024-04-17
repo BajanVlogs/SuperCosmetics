873924d4108312892e46a2031c75d5dd61705333
Lil Kev <kjolly544@gmail.com>
2024-04-17 13:44:54 -0400

    Maybe this

--- src/ninjaknights/SuperCosmetics/forms/SuitForm.php
+++ src/ninjaknights/SuperCosmetics/forms/SuitForm.php
@@ -7,7 +7,7 @@ use ninjaknights\SuperCosmetics\util\skin\SkinUtil;
 use pocketmine\player\Player;
 use jojoe77777\FormAPI\SimpleForm;
 
-class SuitForm
+class SimpleForm extends SuitForm
 {
 
     public function suitForm(Player $player)
